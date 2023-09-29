51dd4fb283a11e7eec0a49e1697b075d91744e10
Rafael Just <justrafito@gmail.com>
2023-09-29 11:46:01 +0200

    Create Neuron class, create GetOutput(); function.

--- Game Over Detection/Game Over Detection.cpp
+++ Game Over Detection/Game Over Detection.cpp
@@ -4,6 +4,37 @@
 #include <iostream>
 using namespace std;
 
+class Neuron
+{
+public:
+	Neuron()
+	{
+		//generate random weight
+		weight = rand();
+	}
+	int weight;
+	int GetOutput(int inputs[])
+	{
+		//calculate wether the neuron will fire or not
+		int total = 0;
+		//add all the weights into the total function
+		for (int i = 0; i < (sizeof(inputs)/sizeof(*inputs)); i++)
+		{
+			int w = inputs[i];
+			total += w;
+		}
+		//calculate wether the neuron will fire or not
+		int result = 0;
+		if (total > (RAND_MAX / 2))
+		{
+			result = weight;
+		}
+		return result;
+	}
+private:
+};
+
+
 int main()
 {
     cout << "Hello World!\n";
