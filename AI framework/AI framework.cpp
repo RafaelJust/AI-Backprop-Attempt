9e28af61526df3a39f8474c36940d0abeae08f24
Rafael Just <justrafito@gmail.com>
2023-09-30 09:36:41 +0200

    Finish basic functional AI framework.

--- AI framework/AI framework.cpp
+++ AI framework/AI framework.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-vector<int> layers {3,5,5,3};
+vector<int> layers {3,5,5,3,4}; //last layer is output layer
 
 class Neuron
 {
@@ -14,7 +14,7 @@ public:
 		cout << "Neuron created with weight " << w << "\n";
 	};
 
-	int GetOutput(vector<int> inputs)
+	int Fire(vector<int> inputs)
 	{
 		//calculate wether the neuron will fire or not
 		int total = 0;
@@ -57,16 +57,30 @@ public:
 		//initialize network
 		for (int layer = 0; layer < layers.size(); layer++)
 		{
-			neurons[layer] = vector<Neuron>(layers[layer], Neuron(rand()));
+			//Create a random value for each neuron
+			for (int n = 0; n < layers[layer]; n++)
+			{
+				neurons[layer].push_back(Neuron(rand()));
+			}
 		}
-		cout << "Network ready to go!";
+		cout << "Network ready to go!\n";
 	};
 	vector<int> GetOutput(vector<int> Input)
 	{
-		for (vector<Neuron> n : neurons)
-		{
+		vector<int> previous = Input;
+		vector<int> Curr;
 
+		for (vector<Neuron> layer : neurons)
+		{
+			//fill in the current vector with the outputs of the neurons in that layer
+			Curr.clear();
+			for (Neuron n : layer)
+			{
+				Curr.push_back(n.Fire(previous));
+			}
+			previous = Curr; //use result for next input
 		}
+		return Curr;
 	}
 };
 
@@ -75,5 +89,10 @@ int main()
 	cout << "Start\n";
 	Network AI;
 	vector<int> i {1,2,3,7,4,34536,8934};
-
+	cout << "AI gives output of";
+	vector<int> o = AI.GetOutput(i);
+	for (int x : o)
+	{
+		cout << " " << x;
+	}
 }
