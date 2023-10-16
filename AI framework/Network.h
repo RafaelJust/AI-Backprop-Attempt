a2490d62df6fa795e903a8bd0f6645ffc4da9bce
Rafael Just <justrafito@gmail.com>
2023-10-16 18:37:36 +0200

    Clean up Network.h by moving the functions outside the class functions.

--- AI framework/Network.h
+++ AI framework/Network.h
@@ -30,21 +30,23 @@ public:
 		cout << "Neuron created with bias " << b << "\n";
 	};
 
-	double Fire(vector<double> inputs)
-	{
-		//add all the weights and previous outputs into the total function
-		double total = 0;
-		for (double i : inputs) 
-		{
-			total += i * weights[i];
-		}
-
-		total += bias;
-		Last_output = (1 / (1 + exp(-total)));
-		cout << "Firing neuron with total of " << total << ", and value " << Last_output << "\n";
-		return Last_output; //Use a sigmoid function to generate an output.
-	}
+	double Fire(vector<double> inputs);
 };
+
+double Neuron::Fire(vector<double> inputs)
+{
+	//add all the weights and previous outputs into the total function
+	double total = 0;
+	for (double i : inputs)
+	{
+		total += i * weights[i];
+	}
+
+	total += bias;
+	Last_output = (1 / (1 + exp(-total)));
+	cout << "Firing neuron with total of " << total << ", and value " << Last_output << "\n";
+	return Last_output; //Use a sigmoid function to generate an output.
+}
 
 vector<double> Substract(vector<double> first, vector<double> second)
 {
@@ -120,55 +122,59 @@ public:
 		}
 		cout << "Network ready to go!\n";
 	};
-	vector<double> GetOutput(vector<double> Input)
-	{
-		vector<double> previous = Input;
-		vector<double> Curr;
 
-		for (vector<Neuron> layer : netw)
-		{
-			//fill in the current vector with the outputs of the neurons in that layer
-			Curr.clear();
-			for (Neuron n : layer)
-			{
-				Curr.push_back(n.Fire(previous));
-			}
-			previous = Curr; //use result for next input
-		}
-		return Curr;
-	}
-
-	void Learn(vector<double> Calculated, vector<double> target)
-	{
-		//Reverse the network temporarily
-		reverse(netw.begin(), netw.end());
-
-		//Output layer
-		//Get the derivatives
-		vector<double> d_error = Substract(Calculated, target);
-		vector<double> d_activation;
-		vector<double> d_weights_output;
-		vector<double> d_biases_output;
-
-		for (double out : Calculated) //d_activation
-		{
-			d_activation.push_back(out * (1 - out));
-		}
-
-		vector<double> delta_output = Multiply(d_error, d_activation);
-
-		//Calculate the gradients of the weights (d_weights_output) and biases (d_biases_output) of the output layer.
-		d_biases_output = delta_output;
-
-		//The gradients are calculated by multiplying the delta_output with the activations of the previous layer (activation_previous_layer)
-		vector<double> activation_previous_layer = GetLastActivations(netw[1]);
-		d_weights_output = Multiply(delta_output, activation_previous_layer);
-
-		//hidden layers
-		bool first = true;
-		for (vector<Neuron> CurrentLayer : netw)
-		{
-			if (first) { first = false; continue; } //skip the first layer, as it is the output layer
-		}
-	}
+	vector<double> GetOutput(vector<double> Input);
+	void Learn(vector<double> Calculated, vector<double> target);
 };
+
+vector<double> Network::GetOutput(vector<double> Input)
+{
+	vector<double> previous = Input;
+	vector<double> Curr;
+
+	for (vector<Neuron> layer : netw)
+	{
+		//fill in the current vector with the outputs of the neurons in that layer
+		Curr.clear();
+		for (Neuron n : layer)
+		{
+			Curr.push_back(n.Fire(previous));
+		}
+		previous = Curr; //use result for next input
+	}
+	return Curr;
+}
+
+void Network::Learn(vector<double> Calculated, vector<double> target)
+{
+	//Reverse the network temporarily
+	reverse(netw.begin(), netw.end());
+
+	//Output layer
+	//Get the derivatives
+	vector<double> d_error = Substract(Calculated, target);
+	vector<double> d_activation;
+	vector<double> d_weights_output;
+	vector<double> d_biases_output;
+
+	for (double out : Calculated) //d_activation
+	{
+		d_activation.push_back(out * (1 - out));
+	}
+
+	vector<double> delta_output = Multiply(d_error, d_activation);
+
+	//Calculate the gradients of the weights (d_weights_output) and biases (d_biases_output) of the output layer.
+	d_biases_output = delta_output;
+
+	//The gradients are calculated by multiplying the delta_output with the activations of the previous layer (activation_previous_layer)
+	vector<double> activation_previous_layer = GetLastActivations(netw[1]);
+	d_weights_output = Multiply(delta_output, activation_previous_layer);
+
+	//hidden layers
+	bool first = true;
+	for (vector<Neuron> CurrentLayer : netw)
+	{
+		if (first) { first = false; continue; } //skip the first layer, as it is the output layer
+	}
+}
